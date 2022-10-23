39cd436bd62a52381db9115d6f35ade73d1cd9ef
王西迪 <wxd237@gmail.com>
2022-10-24 00:42:36 +0800

    Create ToolTemplate.cpp

--- ToolTemplate.cpp
+++ ToolTemplate.cpp
@@ -1,38 +1,15 @@
-//===---- tools/extra/ToolTemplate.cpp - Template for refactoring tool ----===//
-//
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
-//===----------------------------------------------------------------------===//
-//
-//  This file implements an empty refactoring tool using the clang tooling.
-//  The goal is to lower the "barrier to entry" for writing refactoring tools.
-//
-//  Usage:
-//  tool-template <cmake-output-dir> <file1> <file2> ...
-//
-//  Where <cmake-output-dir> is a CMake build directory in which a file named
-//  compile_commands.json exists (enable -DCMAKE_EXPORT_COMPILE_COMMANDS in
-//  CMake to get this output).
-//
-//  <file1> ... specify the paths of files in the CMake source tree. This path
-//  is looked up in the compile command database. If the path of a file is
-//  absolute, it needs to point into CMake's source tree. If the path is
-//  relative, the current working directory needs to be in the CMake source
-//  tree and the file must be in a subdirectory of the current working
-//  directory. "./" prefixes in the relative files will be automatically
-//  removed, but the rest of a relative path must be a suffix of a path in
-//  the compile command line database.
-//
-//  For example, to use tool-template on all files in a subtree of the
-//  source tree, use:
-//
-//    /path/in/subtree $ find . -name '*.cpp'|
-//        xargs tool-template /path/to/build
-//
-//===----------------------------------------------------------------------===//
+// Declares clang::SyntaxOnlyAction.
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Tooling/Tooling.h"
+
 
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
@@ -46,43 +23,157 @@
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Signals.h"
 
-using namespace clang;
-using namespace clang::ast_matchers;
+#include<iostream>
+#include<string>
+
 using namespace clang::tooling;
 using namespace llvm;
+using namespace clang;
 
-namespace {
-class ToolTemplateCallback : public MatchFinder::MatchCallback {
- public:
-  ToolTemplateCallback(Replacements *Replace) : Replace(Replace) {}
+// Apply a custom category to all command-line options so that they are the
+// only ones displayed.
+static llvm::cl::OptionCategory MyToolCategory("clang-ref options");
 
-  void run(const MatchFinder::MatchResult &Result) override {
-    // TODO: This routine will get called for each thing that the matchers find.
-    // At this point, you can examine the match, and do whatever you want,
-    // including replacing the matched text with other text
-    (void)Replace; // This to prevent an "unused member variable" warning;
+// CommonOptionsParser declares HelpMessage with a description of the common
+// command-line options related to the compilation database and input files.
+// It's nice to have this help message in all tools.
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+// A help message for this specific tool can be added afterwards.
+static cl::extrahelp MoreHelp("\nMore help text...\n");
+
+
+void replacespace(char * p,size_t l){
+	for(int i=0;i<l;i++){
+		if(p[i]=='\n')
+		p[i]=' ';
+	}
+}
+
+
+class FindNamedClassVisitor
+  : public RecursiveASTVisitor<FindNamedClassVisitor> {
+public:
+  bool VisitCXXRecordDecl(CXXRecordDecl *Declaration) {
+    // For debugging, dumping the AST nodes will show which nodes are already
+    // being visited.
+   // Declaration->dump();
+
+    // The return value indicates whether we want the visitation to proceed.
+    // Return false to stop the traversal of the AST.
+    return true;
+  }
+  
+
+  std::string curfunction;
+
+  bool VisitFunctionDecl(FunctionDecl * decl){
+    // decl->dump();
+   
+    ASTContext & Context =decl->getASTContext();
+    SourceManager & SM= Context.getSourceManager ();
+    if(SM.isInMainFile(decl->getBeginLoc())==false){
+		return true;
+    }
+	
+    unsigned int paraNum = decl->getNumParams();
+
+
+	
+    const char * b=SM.getCharacterData(decl->getBeginLoc());
+    const char * e=SM.getCharacterData(decl->getBody()->getBeginLoc());
+    size_t len=e-b;
+    char buf[len+1];
+    strncpy(buf,b,len);
+
+    replacespace(buf,len);
+    
+    std::cout<<"defun@:"<<buf<<std::endl;
+    
+	
+
+    auto curFileID=SM.getFileID(decl->getBeginLoc());
+    auto mainFileID=SM.getMainFileID();
+
+    std::string t1=decl->getSourceRange().printToString(SM);
+
+    if(curFileID==mainFileID)
+    	curfunction=decl->getNameInfo().getAsString();
+    else
+	curfunction="";
+
+    //std::cout<<"functiondecl:"<<decl->getNameInfo().getAsString()<<std::endl;
+    //std::cout<<decl->getDeclKindName()<<std::endl;
+    
+    return true;
   }
 
- private:
-  Replacements *Replace;
-};
-} // end anonymous namespace
 
-// Set up the command line options
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
-static cl::OptionCategory ToolTemplateCategory("tool-template options");
+bool VisitCallExpr(CallExpr * expr){
+    // decl->dump();
+    
+    if(expr->getDirectCallee()==nullptr)  return true;
+
+    std::string bcall=expr->getDirectCallee()->getNameInfo().getAsString();
+	
+    if(bcall=="size")  return true;
+    if(bcall=="printf")  return true;
+    if(bcall=="sprintf")  return true;
+    if(bcall=="strcpy")  return true;
+    if(bcall=="strncpy")  return true;
+   
+
+    if(curfunction.size()>0)
+    	std::cout<<"callref@"<<curfunction<<">"<<bcall<<std::endl;
+    
+    
+
+    return true;
+  }
+
+  
+
+
+};
+
+//cmake -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../llvm
+//
+class FindNamedClassConsumer : public clang::ASTConsumer {
+public:
+  virtual void HandleTranslationUnit(clang::ASTContext &Context) {
+    // Traversing the translation unit decl via a RecursiveASTVisitor
+    // will visit all nodes in the AST.
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+private:
+  // A RecursiveASTVisitor implementation.
+  FindNamedClassVisitor Visitor;
+};
+
+class FindNamedClassAction : public clang::ASTFrontendAction {
+public:
+  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+    clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
+    return std::make_unique<FindNamedClassConsumer>();
+  }
+
+
+};
+
+
 
 int main(int argc, const char **argv) {
-  llvm::sys::PrintStackTraceOnErrorSignal();
-  CommonOptionsParser OptionsParser(argc, argv, ToolTemplateCategory);
-  RefactoringTool Tool(OptionsParser.getCompilations(),
-                       OptionsParser.getSourcePathList());
-  ast_matchers::MatchFinder Finder;
-  ToolTemplateCallback Callback(&Tool.getReplacements());
+  auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
+  if (!ExpectedParser) {
+    // Fail gracefully for unsupported options.
+    llvm::errs() << ExpectedParser.takeError();
+    return 1;
+  }
+  CommonOptionsParser& OptionsParser = ExpectedParser.get();
+  ClangTool Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
 
-  // TODO: Put your matchers here.
-  // Use Finder.addMatcher(...) to define the patterns in the AST that you
-  // want to match against. You are not limited to just one matcher!
+ // return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
 
-  return Tool.run(newFrontendActionFactory(&Finder).get());
+  return Tool.run(newFrontendActionFactory<FindNamedClassAction>().get());
 }
